583501aec51ba32ca6e3c305982e76cf133d9601
Dominik <dominik@dominikschuermann.de>
2011-11-02 01:03:46 +0100

    removed old files

--- old/Android.mk
+++
@@ -1,7 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-include $(CLEAR_VARS)
-
-LOCAL_MODULE    := mongoose
-LOCAL_SRC_FILES := main.c mongoose.c
-
-include $(BUILD_EXECUTABLE)
--- org_adaway/jni/Android.mk
+++ org_adaway/jni/Android.mk
@@ -1,3 +1,1 @@
-LOCAL_PATH:= $(call my-dir)
-
 include $(call all-subdir-makefiles)
