0a449c6c0620347616a39fd345495042cff9db41
Dominik <dominik@dominikschuermann.de>
2012-03-20 17:04:07 +0100

    updated wakeful intent service from commonsware

--- org_adaway/src/com/commonsware/cwac/wakeful/AlarmReceiver.java
+++ org_adaway/src/com/commonsware/cwac/wakeful/AlarmReceiver.java
@@ -1,22 +1,19 @@
 /***
-  Copyright (c) 2011 CommonsWare, LLC
-  
-  Licensed under the Apache License, Version 2.0 (the "License"); you may
-  not use this file except in compliance with the License. You may obtain
-  a copy of the License at
-    http://www.apache.org/licenses/LICENSE-2.0
-  Unless required by applicable law or agreed to in writing, software
-  distributed under the License is distributed on an "AS IS" BASIS,
-  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  See the License for the specific language governing permissions and
-  limitations under the License.
+Copyright (c) 2011 CommonsWare, LLC
+Licensed under the Apache License, Version 2.0 (the "License"); you may
+not use this file except in compliance with the License. You may obtain
+a copy of the License at
+http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
  */
 
 package com.commonsware.cwac.wakeful;
 
 import java.io.IOException;
-import org.xmlpull.v1.XmlPullParser;
-import org.xmlpull.v1.XmlPullParserException;
 import android.content.BroadcastReceiver;
 import android.content.ComponentName;
 import android.content.Context;
@@ -26,8 +23,9 @@ import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.content.res.XmlResourceParser;
-import android.util.Log;
 import com.commonsware.cwac.wakeful.WakefulIntentService.AlarmListener;
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
 
 public class AlarmReceiver extends BroadcastReceiver {
     private static final String WAKEFUL_META_DATA = "com.commonsware.cwac.wakeful";
@@ -72,17 +70,17 @@ public class AlarmReceiver extends BroadcastReceiver {
                 xpp.next();
             }
         } catch (NameNotFoundException e) {
-            Log.e(getClass().getName(), "Cannot find own info???", e);
+            throw new RuntimeException("Cannot find own info???", e);
         } catch (XmlPullParserException e) {
-            Log.e(getClass().getName(), "Malformed metadata resource XML", e);
+            throw new RuntimeException("Malformed metadata resource XML", e);
         } catch (IOException e) {
-            Log.e(getClass().getName(), "Could not read resource XML", e);
+            throw new RuntimeException("Could not read resource XML", e);
         } catch (ClassNotFoundException e) {
-            Log.e(getClass().getName(), "Listener class not found", e);
+            throw new RuntimeException("Listener class not found", e);
         } catch (IllegalAccessException e) {
-            Log.e(getClass().getName(), "Listener is not public or lacks public constructor", e);
+            throw new RuntimeException("Listener is not public or lacks public constructor", e);
         } catch (InstantiationException e) {
-            Log.e(getClass().getName(), "Could not create instance of listener", e);
+            throw new RuntimeException("Could not create instance of listener", e);
         }
 
         return (null);
