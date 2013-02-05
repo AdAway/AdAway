d5d3bab3b4b70d2f3a04ad6811cf7276661b5782
Dominik Schürmann <dominik@dominikschuermann.de>
2013-02-05 11:33:14 +0100

    Rework blank_webserver based on new mongoose api

--- AdAway/jni/blank_webserver/blank_webserver.c
+++ AdAway/jni/blank_webserver/blank_webserver.c
@@ -15,60 +15,56 @@ static void WINCDECL signal_handler(int sig_num) {
   exit_flag = sig_num;
 }
 
-static void *callback(enum mg_event event,
-                      struct mg_connection *conn,
-                      const struct mg_request_info *request_info) {
-  if (event == MG_NEW_REQUEST) {
-    // print blank page
-    mg_printf(conn, "HTTP/1.1 200 OK\r\n"
-              "Content-Type: text/plain\r\n\r\n"
-              "", request_info->uri);
-    return "";  // Mark as processed
-  } else {
-    return NULL;
-  }
-}
+// This function will be called by mongoose on every new request.
+static int begin_request_handler(struct mg_connection *conn) {
+  const struct mg_request_info *request_info = mg_get_request_info(conn);
+  char content[100];
 
-// from http://www.enderunix.org/documents/eng/daemon.php
-void daemonize()
-{
-int i,lfp;
-char str[10];
-    if(getppid()==1) return; /* already a daemon */
-    i=fork();
-    if (i<0) exit(1); /* fork error */
-    if (i>0) exit(0); /* parent exits */
-    /* child (daemon) continues */
-    setsid(); /* obtain a new process group */
-}
+  // Prepare the message we're going to send
+  int content_length = snprintf(content, sizeof(content), "");
 
-// don't use 0.0.0.0 as ip address, it will be accessible in the network
-static const char *options[] = {
-  "listening_ports", "127.0.0.1:80,127.0.0.1:443",
-  NULL
-};
+  // Send HTTP reply to the client
+  mg_printf(conn,
+            "HTTP/1.1 200 OK\r\n"
+            "Content-Type: text/plain\r\n"
+            "Content-Length: %d\r\n" // Always set Content-Length
+            "\r\n"
+            "%s",
+            content_length, content);
+
+  // Returning non-zero tells mongoose that our function has replied to
+  // the client, and mongoose should not send client any more data.
+  return 1;
+}
 
 int main(void) {
   struct mg_context *ctx;
+  struct mg_callbacks callbacks;
   
   __android_log_print(ANDROID_LOG_INFO,"AdAway", "Native Webserver: Starting Webserver...");
 
-  daemonize();
-  
-  /* Setup signal handler: quit on Ctrl-C */
+  // List of options. Last element must be NULL.
+  // don't use 0.0.0.0 as ip address, it will be accessible in the network
+  const char *options[] = {"listening_ports", "127.0.0.1:80,127.0.0.1:443", NULL};
+
+  // Prepare callbacks structure. We have only one callback, the rest are NULL.
+  memset(&callbacks, 0, sizeof(callbacks));
+  callbacks.begin_request = begin_request_handler;
+
+  // Setup signal handler: quit on Ctrl-C
   signal(SIGTERM, signal_handler);
   signal(SIGINT, signal_handler);
-
-  /* Start mongoose */
-  ctx = mg_start(&callback, NULL, options);
+  
+  // Start mongoose
+  ctx = mg_start(&callbacks, NULL, options);
   
   __android_log_print(ANDROID_LOG_INFO,"AdAway", "Native Webserver: Webserver started!");
-  
+
   while (exit_flag == 0) {
     sleep(1);
   }
   
-  /* Stop mongoose */
+  // Stop mongoose
   __android_log_print(ANDROID_LOG_INFO,"AdAway", "Native Webserver: Exiting on signal %d, waiting for all threads to finish...", exit_flag);
   fflush(stdout);
   mg_stop(ctx);
