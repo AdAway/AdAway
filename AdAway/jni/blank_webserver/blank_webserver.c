398f9cf987b9576c00c917199375fb8879e2e32e
Dominik Schürmann <dominik@dominikschuermann.de>
2013-02-05 13:28:36 +0100

    prevent webserver from being killed on low memory by setting oom_adj

--- AdAway/jni/blank_webserver/blank_webserver.c
+++ AdAway/jni/blank_webserver/blank_webserver.c
@@ -4,15 +4,50 @@
 #include <signal.h>
 #include <android/log.h>
 
+#include <errno.h>
+
 #include <sys/wait.h>
 #define WINCDECL
 
 #include "mongoose.h"
 
+#define THIS_FILE "AdAway"
+
 static int exit_flag;
 
 static void WINCDECL signal_handler(int sig_num) {
   exit_flag = sig_num;
+}
+
+#define OOM_ADJ_PATH	"/proc/self/oom_adj"
+/*
+ * The magic "don't kill me", as documented in eg:
+ * http://lxr.linux.no/#linux+v2.6.32/Documentation/filesystems/proc.txt
+ */
+#define OOM_ADJ_NOKILL	-17
+
+static int oom_adj_save = INT_MIN;
+
+/*
+ * Tell the kernel's out-of-memory killer to avoid sshd.
+ * Returns the previous oom_adj value or zero.
+ */
+void oom_adjust_setup(void)
+{
+	FILE *fp;
+
+	if ((fp = fopen(OOM_ADJ_PATH, "r+")) != NULL) {
+		if (fscanf(fp, "%d", &oom_adj_save) != 1)
+			__android_log_print(ANDROID_LOG_INFO, THIS_FILE, "error reading %s: %s", OOM_ADJ_PATH, strerror(errno));
+		else {
+			rewind(fp);
+			if (fprintf(fp, "%d\n", OOM_ADJ_NOKILL) <= 0)
+				__android_log_print(ANDROID_LOG_INFO, THIS_FILE, "error writing %s: %s", OOM_ADJ_PATH, strerror(errno));
+			else
+				__android_log_print(ANDROID_LOG_INFO, THIS_FILE, "Set %s from %d to %d", OOM_ADJ_PATH, oom_adj_save, OOM_ADJ_NOKILL);
+		}
+		fclose(fp);
+	}
 }
 
 // This function will be called by mongoose on every new request.
@@ -41,7 +76,14 @@ int main(void) {
   struct mg_context *ctx;
   struct mg_callbacks callbacks;
   
-  __android_log_print(ANDROID_LOG_INFO,"AdAway", "Native Webserver: Starting Webserver...");
+  __android_log_print(ANDROID_LOG_INFO, THIS_FILE, "Native Webserver: Starting Webserver...");
+  
+  pid_t pid;
+  pid = getpid();
+  
+  __android_log_print(ANDROID_LOG_INFO, THIS_FILE, "Native Webserver: pid: %d", pid);
+  
+  oom_adjust_setup();
 
   // List of options. Last element must be NULL.
   // don't use 0.0.0.0 as ip address, it will be accessible in the network
@@ -58,17 +100,17 @@ int main(void) {
   // Start mongoose
   ctx = mg_start(&callbacks, NULL, options);
   
-  __android_log_print(ANDROID_LOG_INFO,"AdAway", "Native Webserver: Webserver started!");
+  __android_log_print(ANDROID_LOG_INFO, THIS_FILE, "Native Webserver: Webserver started!");
 
   while (exit_flag == 0) {
     sleep(1);
   }
   
   // Stop mongoose
-  __android_log_print(ANDROID_LOG_INFO,"AdAway", "Native Webserver: Exiting on signal %d, waiting for all threads to finish...", exit_flag);
+  __android_log_print(ANDROID_LOG_INFO, THIS_FILE, "Native Webserver: Exiting on signal %d, waiting for all threads to finish...", exit_flag);
   fflush(stdout);
   mg_stop(ctx);
-  __android_log_print(ANDROID_LOG_INFO,"AdAway", "Native Webserver: done.\n", exit_flag);
+  __android_log_print(ANDROID_LOG_INFO, THIS_FILE, "Native Webserver: done.\n", exit_flag);
 
   return 0;
 }
