3772627e115e18d30ac6a1a1c4f20fd3630827b4
Kaladin Light <0.kaladin@gmail.com>
2014-12-22 11:38:14 -0500

    Adding back x86 mips

--- AdAway/jni/Application.mk
+++ AdAway/jni/Application.mk
@@ -2,7 +2,7 @@
 APP_OPTIM := release
 
 # Build target
-APP_ABI := armeabi
+APP_ABI := armeabi x86 mips
 #APP_ABI := armeabi armeabi-v7a x86 mips
 
 # If APP_MODULES is not set, all modules are compiled!
