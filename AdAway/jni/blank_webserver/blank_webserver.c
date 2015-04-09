3872b2232ffb9f113ddecb9298b142c481d3a3e1
Dominik Schürmann <dominik@dominikschuermann.de>
2015-04-09 23:22:27 +0200

    Re-add oom killer prevention to webserver

--- AdAway/jni/blank_webserver/blank_webserver.c
+++ AdAway/jni/blank_webserver/blank_webserver.c
@@ -1,12 +1,12 @@
-// Copyright (c) 2014 Cesanta Software
-// All rights reserved
-//
-// This example demostrates basic use of Mongoose embedded web server.
-// $Date: 2014-09-09 22:20:23 UTC $
-
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <signal.h>
+#include <android/log.h>
+#include <errno.h>
 #include "mongoose.h"
+
+#define THIS_FILE "AdAway"
 
 static const char *empty_html =
   " ";
@@ -28,21 +28,55 @@ static int ev_handler(struct mg_connection *conn, enum mg_event ev) {
   }
 }
 
+
+#define OOM_ADJ_PATH	"/proc/self/oom_adj"
+/*
+* The magic "don't kill me", as documented in eg:
+* http://lxr.linux.no/#linux+v2.6.32/Documentation/filesystems/proc.txt
+*/
+#define OOM_ADJ_NOKILL	-17
+static int oom_adj_save = INT_MIN;
+/*
+* Tell the kernel's out-of-memory killer to avoid this process.
+* Returns the previous oom_adj value or zero.
+*/
+void oom_adjust_setup(void) {
+  FILE *fp;
+  if ((fp = fopen(OOM_ADJ_PATH, "r+")) != NULL) {
+    if (fscanf(fp, "%d", &oom_adj_save) != 1)
+      __android_log_print(ANDROID_LOG_INFO, THIS_FILE, "error reading %s: %s", OOM_ADJ_PATH, strerror(errno));
+    else {
+      rewind(fp);
+      if (fprintf(fp, "%d\n", OOM_ADJ_NOKILL) <= 0)
+        __android_log_print(ANDROID_LOG_INFO, THIS_FILE, "error writing %s: %s", OOM_ADJ_PATH, strerror(errno));
+      else
+        __android_log_print(ANDROID_LOG_INFO, THIS_FILE, "Set %s from %d to %d", OOM_ADJ_PATH, oom_adj_save, OOM_ADJ_NOKILL);
+    }
+    fclose(fp);
+  }
+}
+
 int main(void) {
   struct mg_server *server;
+
+  __android_log_print(ANDROID_LOG_INFO, THIS_FILE, "Native Webserver: Starting Webserver...");
+
+  oom_adjust_setup();
 
   // Create and configure the server
   server = mg_create_server(NULL, ev_handler);
   mg_set_option(server, "listening_port", "127.0.0.1:80,127.0.0.1:443");
 
   // Serve request. Hit Ctrl-C to terminate the program
-  printf("Starting on port %s\n", mg_get_option(server, "listening_port"));
+  __android_log_print(ANDROID_LOG_INFO, THIS_FILE, "Native Webserver: Webserver started!");
   for (;;) {
     mg_poll_server(server, 1000);
   }
 
   // Cleanup, and free server instance
   mg_destroy_server(&server);
+
+  __android_log_print(ANDROID_LOG_INFO, THIS_FILE, "Native Webserver: exited");
 
   return 0;
 }
