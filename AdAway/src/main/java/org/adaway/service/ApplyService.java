d908976c32c53feda1a9a86b3288e23bb0e70a16
Kaladin Light <0.kaladin@gmail.com>
2016-01-08 12:15:18 -0500

    Add generation timestamp into header per some forum requests.

--- AdAway/src/main/java/org/adaway/service/ApplyService.java
+++ AdAway/src/main/java/org/adaway/service/ApplyService.java
@@ -33,6 +33,8 @@ import java.net.URL;
 import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Date;
+import java.text.SimpleDateFormat;
 
 import org.adaway.R;
 import org.adaway.helper.PreferenceHelper;
@@ -322,9 +324,13 @@ public class ApplyService extends WakefulIntentService {
 
             bos = new BufferedOutputStream(fos);
 
+            // build current timestamp for header
+            SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            Date now = new Date();
+
             // add adaway header
-            String header = Constants.HEADER1 + Constants.LINE_SEPERATOR + Constants.HEADER2
-                    + Constants.LINE_SEPERATOR + Constants.HEADER_SOURCES;
+            String header = Constants.HEADER1 + formatter.format(now) + Constants.LINE_SEPERATOR +
+                    Constants.HEADER2 + Constants.LINE_SEPERATOR + Constants.HEADER_SOURCES;
             bos.write(header.getBytes());
 
             // write sources into header
