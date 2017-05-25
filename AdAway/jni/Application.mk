908e95a1f96f62c737a983a623002a4ef882ca56
PerfectSlayer <bruce.bujon@gmail.com>
2017-05-25 16:06:37 +0200

    Update makefile for the latest toolchain

--- AdAway/jni/Application.mk
+++ AdAway/jni/Application.mk
@@ -6,3 +6,6 @@ APP_ABI := all
 
 # If APP_MODULES is not set, all modules are compiled!
 APP_MODULES := blank_webserver tcpdump
+
+# Allow undefined modules (cutils log) with latest NDK toolchain
+APP_ALLOW_MISSING_DEPS=true
