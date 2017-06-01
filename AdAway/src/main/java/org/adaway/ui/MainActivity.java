41648239448237e57a948b434423bff4461ce73c
PerfectSlayer <bruce.bujon@gmail.com>
2017-06-01 21:41:43 +0200

    Add menu drawer navigation implementation

--- AdAway/src/main/java/org/adaway/ui/MainActivity.java
+++ AdAway/src/main/java/org/adaway/ui/MainActivity.java
@@ -24,17 +24,22 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.app.ListFragment;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import org.adaway.R;
+import org.adaway.helper.OpenHelper;
 import org.adaway.helper.ResultHelper;
 import org.adaway.util.Constants;
 import org.adaway.util.Log;
@@ -48,6 +53,9 @@ public class MainActivity extends AppCompatActivity {
     Activity mActivity;
     private DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle mDrawerToggle;
+    private ListView mDrawerList;
+    private CharSequence mTitle;
+    private CharSequence mDrawerTitle;
 
 
     /**
@@ -96,30 +104,48 @@ public class MainActivity extends AppCompatActivity {
          */
         // Configure drawer items
         String[] mPlanetTitles = getResources().getStringArray(R.array.drawer_items);
-        ListView mDrawerList = (ListView) findViewById(R.id.left_drawer);
+        mDrawerList = (ListView) findViewById(R.id.left_drawer);
         mDrawerList.setAdapter(
                 new ArrayAdapter<>(this, R.layout.drawer_list_item, mPlanetTitles)
         );
         // Set drawer item listener
         // Set the list's click listener
-//        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
-// TODO
+        mDrawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                selectDrawerMenuItem(position);
+            }
+        });
 
         // Configure drawer toggle
+        mTitle = mDrawerTitle = getTitle();
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         mDrawerToggle = new ActionBarDrawerToggle(
                 this,                  /* host Activity */
                 mDrawerLayout,         /* DrawerLayout object */
                 R.string.drawer_open,  /* "open drawer" description */
                 R.string.drawer_close  /* "close drawer" description */
-        );
+        ) {
+            /** Called when a drawer has settled in a completely closed state. */
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                getSupportActionBar().setTitle(mTitle);
+            }
+
+            /** Called when a drawer has settled in a completely open state. */
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                getSupportActionBar().setTitle(mDrawerTitle);
+            }
+
+        };
         mDrawerLayout.addDrawerListener(mDrawerToggle);
         /*
          * Configure actionbar.
          */
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) {
-            actionBar.setSubtitle(R.string.app_subtitle);
+//            actionBar.setSubtitle(R.string.app_subtitle);
             actionBar.setDisplayHomeAsUpEnabled(true);
             actionBar.setHomeButtonEnabled(true);
         }
@@ -135,6 +161,8 @@ public class MainActivity extends AppCompatActivity {
             HomeFragment homeFragment = new HomeFragment();
             fragmentTransaction.replace(R.id.content_frame, homeFragment);
             fragmentTransaction.commit();
+            // Select home menu item
+            mDrawerList.setItemChecked(0, true);
         }
     }
 
@@ -161,5 +189,69 @@ public class MainActivity extends AppCompatActivity {
         // Handle your other action bar items...
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void setTitle(CharSequence title) {
+        mTitle = title;
+        getSupportActionBar().setTitle(mTitle);
+    }
+
+    /**
+     * Swaps fragments in the main content view.
+     *
+     * @param position The position of the selected item in drawer menu.
+     */
+    private void selectDrawerMenuItem(int position) {
+        Fragment fragment = null;
+        switch (position) {
+            case 0:
+                fragment = new HomeFragment();
+                break;
+            case 1:
+                fragment = new HostsSourcesFragment();
+                break;
+            case 2:
+                fragment = new ListFragment();
+                break;
+            case 3:
+                OpenHelper.openHostsFile(this);
+                break;
+            case 4:
+                fragment = new WebserverFragment();
+                break;
+            case 5:
+                fragment = new ScanAdwareListFragment();
+                break;
+            case 6:
+                fragment = new PrefsFragment();
+                break;
+        }
+
+        // Create a new fragment and specify the planet to show based on position
+//        Bundle args = new Bundle();
+//        args.putInt(PlanetFragment.ARG_PLANET_NUMBER, position);
+//        fragment.setArguments(args);
+
+        if (fragment != null) {
+            // Insert the fragment by replacing any existing fragment
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            fragmentManager.beginTransaction()
+                    .replace(R.id.content_frame, fragment)
+                    .commit();
+        } else {
+            return;
+        }
+
+        // Highlight the selected item, update the title, and close the drawer
+        mDrawerList.setItemChecked(position, true);
+        String itemName;
+        if (position == 0) {
+            itemName = getString(R.string.app_name);
+        } else {
+            itemName = getResources().getStringArray(R.array.drawer_items)[position];
+        }
+        setTitle(itemName);
+        mDrawerLayout.closeDrawer(mDrawerList);
     }
 }
