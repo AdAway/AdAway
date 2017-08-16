9571a3c5f9855eab71a79577ac24e2e23d7985e5
PerfectSlayer <bruce.bujon@gmail.com>
2017-08-16 21:47:30 +0200

    Finish deployment configuration
    The debug APK is uploaded to Google Cloud Storage using gsutil and interoperability API.
    The gsutil command is retrieved from and install from Google storage at each build.
    The configuration is a boto configuration file, located in home folder.
    It is uncrypted before deployment removed just removed after.
    The deployment script, deploy.sh, should work for any fork but credentials to uncrypt it are not shared (due to Google Cloud Platform posible abuse).
    
    NOTE: I did some force pushes on this commit in order to trigger the CI as I didn't want to fill my history with all the tries. It should end now.

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ wget https://storage.googleapis.com/pub/gsutil.tar.gz
 # Install gsutil
 tar xfz gsutil.tar.gz -C $HOME
 # Copy APK to bucket
-~/gsutil/gsutil /home/travis/build/PerfectSlayer/AdAway/AdAway/build/outputs/apk/AdAway-debug.apk gs://build-repository/
+~/gsutil/gsutil cp AdAway/build/outputs/apk/AdAway-debug.apk gs://build-repository/
 # Allow everyone to read APK
 ~/gsutil/gsutil acl ch -u AllUsers:R gs://build-repository/AdAway-debug.apk
 
