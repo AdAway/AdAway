9ea88c2574ed09559db744bc80c02d8e0d4fbf66
PerfectSlayer <bruce.bujon@gmail.com>
2017-08-24 22:12:18 +0200

    Fix typo (whitespace)

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 wget https://storage.googleapis.com/pub/gsutil.tar.gz
 # Install gsutil
 tar xfz gsutil.tar.gz -C $HOME
-# Copy APK to bucket
+# Copy APK to bucket
 ~/gsutil/gsutil cp AdAway/build/outputs/apk/AdAway-debug.apk gs://build-repository/
 # Allow everyone to read APK
 ~/gsutil/gsutil acl ch -u AllUsers:R gs://build-repository/AdAway-debug.apk
