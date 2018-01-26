b3c98947a26c0518666014861717c383304675f4
Joshndroid <fmxjoshiewah@gmail.com>
2018-01-26 18:10:38 +1000

    buildapk.sh : update for rom integration

--- buildapk.sh
+++ buildapk.sh
@@ -8,4 +8,7 @@ cd ..
 ./gradlew build
 #Now to sign the apks
 java -jar ~/uber-apk-signer-0.8.3.jar --apks /media/joshndroid/scratch/AdAway/AdAway/build/outputs/apk
+#Lets make the apk easier to integrate with a rom as a prebuilt
+cd /media/joshndroid/scratch/AdAway/AdAway/build/outputs/apk/
+cp AdAway-release-aligned-debugSigned.apk AdAway.apk
 exit
