ccae7ed66615d18fc2b0a19e6b54d0318c1000df
PerfectSlayer <bruce.bujon@gmail.com>
2018-02-14 20:23:49 +0100

    Add sign script

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,11 @@ wget https://storage.googleapis.com/pub/gsutil.tar.gz
 tar xfz gsutil.tar.gz -C $HOME
 # Define configuration file
 export BOTO_CONFIG=.boto
-# Copy APK to bucket
-~/gsutil/gsutil cp AdAway/build/outputs/apk/AdAway-debug.apk gs://build-repository/
-# Allow everyone to read APK
+# Copy debug APK to bucket
+~/gsutil/gsutil cp AdAway/build/outputs/apk/debug/AdAway-debug.apk gs://build-repository/
+# Copy release APK to bucket
+~/gsutil/gsutil cp AdAway/build/outputs/apk/release/AdAway-release.apk gs://build-repository/
+# Allow everyone to read APKs
 ~/gsutil/gsutil acl ch -u AllUsers:R gs://build-repository/AdAway-debug.apk
+~/gsutil/gsutil acl ch -u AllUsers:R gs://build-repository/AdAway-release.apk
+
