5150aff2b11cbc0ca8277fd5dabd3beef5d48ccd
PerfectSlayer <bruce.bujon@gmail.com>
2018-08-09 21:46:30 +0200

    Fix crash when adding already existing hosts source

--- app/src/main/java/org/adaway/db/dao/HostsSourceDao.java
+++ app/src/main/java/org/adaway/db/dao/HostsSourceDao.java
@@ -4,6 +4,7 @@ import android.arch.lifecycle.LiveData;
 import android.arch.persistence.room.Dao;
 import android.arch.persistence.room.Delete;
 import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.OnConflictStrategy;
 import android.arch.persistence.room.Query;
 import android.arch.persistence.room.Update;
 
@@ -19,7 +20,7 @@ import java.util.List;
  */
 @Dao
 public interface HostsSourceDao {
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
     void insert(HostsSource source);
 
     @Update
