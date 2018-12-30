c02199f4fc6fb23395ba813b5a9b1d6c96c7411a
PerfectSlayer <bruce.bujon@gmail.com>
2018-12-30 09:01:30 +0100

    Simplify webserver build script

--- webserver/jni/Android.mk
+++ webserver/jni/Android.mk
@@ -1,30 +1,11 @@
-# BUILD MONGOOSE
 
 LOCAL_PATH := $(call my-dir)/mongoose
 
 include $(CLEAR_VARS)
 
 LOCAL_CFLAGS	       := -D MG_ENABLE_IPV6 -std=c99 -O2 -W -Wall -pthread -pipe $(COPT)
-LOCAL_MODULE           := mongoose
+LOCAL_MODULE           := blank_webserver
 LOCAL_SRC_FILES        := ../blank_webserver.c mongoose.c
-
-include $(BUILD_STATIC_LIBRARY)
-
-
-# BUILD BLANK WEBSERVER
-
-include $(CLEAR_VARS)
-
-LOCAL_PATH := $(LOCAL_PATH)/..
-
-LOCAL_MODULE := blank_webserver
-
-LOCAL_SRC_FILES := blank_webserver.c
-LOCAL_CFLAGS += -pie -fPIE
-LOCAL_LDFLAGS += -pie -fPIE
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/mongoose
-LOCAL_STATIC_LIBRARIES := mongoose
-
-LOCAL_LDLIBS := -llog
+LOCAL_LDLIBS           := -llog
 
 include $(BUILD_EXECUTABLE)
