9cbd19af37ff03d53449fa9eba71441435d16ba2
PerfectSlayer <bruce.bujon@gmail.com>
2018-12-30 09:47:06 +0100

    Create stub lib to ship it to aar
    Create a stub library to declare it in order to be shipped into the aar by gradle
    Then build the real executable
    Then gradle script replace stub library binary with executable binary.
    Then the aar is assemble with executable binary.

--- webserver/jni/Android.mk
+++ webserver/jni/Android.mk
@@ -1,5 +1,18 @@
+# Build stub library in order to declare shared library to include to AAR
 
-LOCAL_PATH := $(call my-dir)/mongoose
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE           := blank_webserver_exec
+LOCAL_SRC_FILES        := stub.c
+
+include $(BUILD_SHARED_LIBRARY)
+
+
+# Build webserver executable
+
+LOCAL_PATH := $(LOCAL_PATH)/mongoose
 
 include $(CLEAR_VARS)
 
@@ -9,3 +22,5 @@ LOCAL_SRC_FILES        := ../blank_webserver.c mongoose.c
 LOCAL_LDLIBS           := -llog
 
 include $(BUILD_EXECUTABLE)
+
+# Stub library binary is replace by webserver executable at the end of ndkbuild by gradle build script
