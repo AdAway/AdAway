41cb73269c76a6aaa259e7a8e064b59cce1af939
PerfectSlayer <bruce.bujon@gmail.com>
2020-05-29 10:37:18 +0200

    feat(webserver): Use OpenSSL from Google prefab

--- webserver/jni/Android.mk
+++ webserver/jni/Android.mk
@@ -3,34 +3,6 @@ LOCAL_PATH := $(call my-dir)
 
 
 #
-# Import crypto prebuilt library
-# Prebuilt from https://mvnrepository.com/artifact/com.android.ndk.thirdparty/openssl/1.1.1g-alpha-1
-#
-
-include $(CLEAR_VARS)
-
-LOCAL_MODULE := crypto
-LOCAL_SRC_FILES := crypto/libs/android.$(TARGET_ARCH_ABI)/libcrypto.so
-LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/crypto/include
-
-include $(PREBUILT_SHARED_LIBRARY)
-
-
-#
-# Import openssl prebuilt library
-# Prebuilt from https://mvnrepository.com/artifact/com.android.ndk.thirdparty/openssl/1.1.1g-alpha-1
-#
-
-include $(CLEAR_VARS)
-
-LOCAL_MODULE := ssl
-LOCAL_SRC_FILES := ssl/libs/android.$(TARGET_ARCH_ABI)/libssl.so
-LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/ssl/include
-
-include $(PREBUILT_SHARED_LIBRARY)
-
-
-#
 # Build stub library in order to declare shared library to include to AAR
 #
 
@@ -59,3 +31,9 @@ include $(BUILD_EXECUTABLE)
 #
 # NOTE: Stub library binary is replaced by webserver executable at the end of ndkbuild by gradle build script
 #
+
+
+#
+# Import OpenSSL (ssl and crypto) libraries from prefab
+#
+$(call import-module,prefab/openssl)
