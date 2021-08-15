34e3bcd7a0a4e3fa6f19653a84013495dc324d2e
PerfectSlayer <bruce.bujon@gmail.com>
2021-08-15 09:59:12 +0200

    fix(webserver): Set max validity time to 37 months
    Fix #2875

--- Resources/certificate/generate.sh
+++ Resources/certificate/generate.sh
@@ -1,3 +1,3 @@
 rm localhost.*
-openssl req -x509 -out localhost.crt -keyout localhost.key -newkey rsa:2048 -nodes -sha256 -days 3650 -subj /CN=localhost -extensions EXT -config ssl.conf
 
+openssl req -x509 -out localhost.crt -keyout localhost.key -newkey rsa:2048 -nodes -sha256 -days 1126 -subj /CN=localhost -extensions EXT -config ssl.conf
