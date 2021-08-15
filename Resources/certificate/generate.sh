730aae04e9a1b7229f6ace29a62cf796784db719
PerfectSlayer <bruce.bujon@gmail.com>
2021-08-15 10:00:14 +0200

    feat(webserver): Add support for MINGW

--- Resources/certificate/generate.sh
+++ Resources/certificate/generate.sh
@@ -1,3 +1,9 @@
 rm localhost.*
 
+# Add compatiblity for MINGW
+kernel=$(uname -s)
+if [[ $kernel == MINGW* ]]; then
+  export MSYS_NO_PATHCONV=1
+fi
+
 openssl req -x509 -out localhost.crt -keyout localhost.key -newkey rsa:2048 -nodes -sha256 -days 1126 -subj /CN=localhost -extensions EXT -config ssl.conf
