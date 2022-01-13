f98ff62993342a83a6f2b1daaa6ad07c72e00c60
lindwurm <lindwurm.q@gmail.com>
2022-01-14 06:30:26 +0900

    fix(webserver): Set max validity time to 398 days
    * Additional fix for #2875
    
    Signed-off-by: lindwurm <lindwurm.q@gmail.com>

--- Resources/certificate/generate.sh
+++ Resources/certificate/generate.sh
@@ -6,4 +6,4 @@ if [[ $kernel == MINGW* ]]; then
   export MSYS_NO_PATHCONV=1
 fi
 
-openssl req -x509 -out localhost.crt -keyout localhost.key -newkey rsa:2048 -nodes -sha256 -days 1126 -subj /CN=localhost -extensions EXT -config ssl.conf
+openssl req -x509 -out localhost.crt -keyout localhost.key -newkey rsa:2048 -nodes -sha256 -days 398 -subj /CN=localhost -extensions EXT -config ssl.conf
