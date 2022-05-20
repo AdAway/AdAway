8dce8e73793b3ed824c9531733ea19a95fce18de
PerfectSlayer <bruce.bujon@gmail.com>
2022-05-20 10:35:35 +0200

    feat(root): Add package documentation

---
+++ app/src/main/java/org/adaway/model/root/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * The root ad-blocker related implementations.
+ *
+ * It relies on libsu from John Wu and shell commands.
+ * Supported shell commands are described here: https://chromium.googlesource.com/aosp/platform/system/core/+/upstream/shell_and_utilities/#android-8_0-oreo
+ *
+ * @author Bruce BUJON (bruce.bujon(at)gmail(dot)com)
+ */
+package org.adaway.model.root;
